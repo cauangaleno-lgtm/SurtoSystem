defca1458af975e822ee78582e88d5422b58969f
cauangaleno-lgtm <cauan.galeno@aluno.senai.br>
2025-11-21 10:56:20 -0300

    v6

---
+++ script/dashscript.js
@@ -0,0 +1,5 @@
+const navdashbut = document.getElementById("butnav1");
+const navdash = document.getElementById("navnav1");
+navdashbut.addEventListener("click", () => {
+    navdash.classList.toggle("aberto");
+})
--- script/script.js
+++ script/script.js
@@ -3,3 +3,15 @@ const itens = document.getElementById("itens");
 btn.addEventListener("click", () => {
     itens.classList.toggle("aberto");
 })
+
+const navbtn = document.getElementById("butnav");
+const navna = document.getElementById("navnav2");
+navbtn.addEventListener("click", () => {
+    navna.classList.toggle("aberto");
+})
+
+const navdashbut = document.getElementById("butnav1");
+const navdash = document.getElementById("navnav1");
+navdashbut.addEventListener("click", () => {
+    navdash.classList.toggle("aberto");
+})
